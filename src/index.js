4263bbe021094f17580df00d95a5656d12b86844
Dmitry Natalchuk <next1996@ukr.net>
2022-08-12 17:44:31 +0300

    fix hw work 11

--- src/index.js
+++ src/index.js
@@ -8,50 +8,66 @@ import { renderGalleru } from "./js/renderGalleru";
 const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
 const loadMore = document.querySelector('.load-more');
-loadMore.classList.add("is-hidden");
 
 let query = '';
 let page = 1;
 let simpleLightBox;
 const perPage = 40;
+loadMore.classList.add("is-hidden");
 
 form.addEventListener("submit",onSearchForm);
 loadMore.addEventListener('click', onLoadMoreNextPage);
 
 
 function onSearchForm (event) {
-    event.preventDefault()
-    page = 1
-    query = event.currentTarget.searchQuery.value.trim()
-    gallery.innerHTML = ""
+    event.preventDefault();
+    page = 1;
+    query = event.currentTarget.searchQuery.value.trim();
+    gallery.innerHTML = "";
     
-
     if(query === "") {
+        loadMore.classList.add("is-hidden");
         return Notify.failure('The search string cannot be empty. Please specify your search query.')
     }
 
 fetchImgApi(query, page, perPage).then(({data}) => {
-    if(data.totalHits === 0) {
+    if(data.totalHits === 0 ) {
+        loadMore.classList.add("is-hidden");
         return Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-    } else {
+     } else {
         renderGalleru(data.hits)
-        loadMore.classList.remove("is-hidden");
         simpleLightBox = new SimpleLightbox('.gallery a').refresh()
         Notify.success(`Hooray! We found ${data.totalHits} images.`)
+        if (data.totalHits < perPage && data.totalHits === "") {
+            loadMore.classList.add("is-hidden");
+            return
+          }
+          return loadMore.classList.remove("is-hidden");
     }
   }).catch(error => console.log(error))
+    .finally(() => {
+        form.reset()
+    });
 }
 
 function onLoadMoreNextPage () {
   page += 1
+  
   simpleLightBox.destroy()
 fetchImgApi(query, page, perPage).then(({data}) => {
   if(data.totalHits === 0) {
       return Notify.failure('Sorry, there are no images matching your search query. Please try again.')
   } else {
       renderGalleru(data.hits)
-      simpleLightBox = new SimpleLightbox('.search__gallery a').refresh()
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh()
       Notify.success(`Hooray! Left until the end ${data.totalHits - perPage * (page - 1)} images.`)
+      const totalPages = Math.ceil(data.totalHits / perPage);
+      if(page > totalPages) {
+        loadMore.classList.add("is-hidden");
+        Notify.failure("We're sorry, but you've reached the end of search results.")
+        return
+      }
+      return loadMore.classList.remove("is-hidden")
   }
 }).catch(error => console.log(error))
 }
